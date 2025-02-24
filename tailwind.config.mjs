816c1ac99c45263e7f516c3cffb865321b9b41c0
hitalin <hitalin@proton.me>
2025-02-24 16:42:17 +0900

    fix: Adjust color scheme to be similar to the latest Misskey color theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,29 +2,29 @@
 import starlightPlugin from '@astrojs/starlight-tailwind';
 
 const accent = {
-  200: '#D4D0FF',
-  600: '#C242C8', // ライトテーマのボタン
-  900: '#372D7A',
-  950: '#211F40',
+  200: '#DB9DFF',
+  600: '#A398FF', // ライトテーマのボタン
+  900: '#8E78FF',
+  950: '#4B38A5',
 };
 
 const gray = {
-  100: '#E6F8FF', // ライトテーマのバー色
-  200: '#F4F6F8',
-  300: '#A8B1C2',
-  400: '#8490A5',
-  500: '#362852', // ライトテーマの文章色
-  700: '#251A3B',
-  800: '#0F0A22', // ダークテーマのバー色
-  900: '#0D0920', // ダークテーマの背景色
+  100: '#DB9DFF', // ライトテーマのバーの色
+  200: '#FCF5FF',
+  300: '#A8B1C2', // ダークテーマの文字色
+  400: '#B4BDD0',
+  500: '#251B38', // ライトテーマの説明文色
+  700: '#16102C', // ライトテーマの文章色
+  800: '#16102C', // ダークテーマの背景色
+  900: '#0D0920', // ダークテーマの最も暗い色
 };
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-    './node_modules/@astrojs/starlight/**/*.{js,astro}', // 追加
-    './node_modules/@astrojs/starlight-tailwind/**/*.{js,astro}', // 追加
+    './node_modules/@astrojs/starlight/**/*.{js,astro}',
+    './node_modules/@astrojs/starlight-tailwind/**/*.{js,astro}',
   ],
   theme: {
     extend: {
